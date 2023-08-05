74be0c641f8954ebc0c2ca7ced7b97ad68cc7af5
Guilherme <guilherme@gmail.com>
2023-08-05 12:53:13 -0300

    Criando model ProductModel

---
+++ ContentContext/ProductModel.cs
@@ -0,0 +1,10 @@
+namespace ContentContext{
+    public class ProductModel{
+        public ProductModel()
+        {
+            Id = Guid.NewGuid();
+        }
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ namespace Store{
     class Program{
         static void Main(string[] args){
             Console.WriteLine("O que é que tem");
-            
+
             for(int i = 0; i < 10; i++){
                 Console.WriteLine("nova alteração");
             }
