16aa213244bfef9fd4dce87c1f62e1e72d3e7318
Guilherme <guilherme@gmail.com>
2023-08-05 12:49:01 -0300

    Criando o for

--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@ namespace Store{
     class Program{
         static void Main(string[] args){
             Console.WriteLine("O que é que tem");
+            
+            for(int i = 0; i < 10; i++){
+                Console.WriteLine("nova alteração");
+            }
         }
     }
 }
